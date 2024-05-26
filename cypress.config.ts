e84a9bfed83fb6712d4bce527e47519db9c40b13
eDin2 <elvedin.cajlakovic@gmail.com>
2024-05-26 13:42:02 +0200

    define config (deafault)

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,16 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    baseUrl: "https://www.saucedemo.com/",
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    watchForFileChanges: false,
+    defaultCommandTimeout: 10000,
+    screenshotOnRunFailure: true,
+    video: true,
+    trashAssetsBeforeRuns: false,
+    testIsolation: false,
+    chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
